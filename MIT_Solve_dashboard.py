d8ca81ccc3c2c549ed26446ce7d0f8aa83d81ec2
Ryan Donlan <donlan.ryan@gmail.com>
2020-07-09 12:39:07 -0400

    pushing updates

--- MIT_Solve_dashboard.py
+++ MIT_Solve_dashboard.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 
+
 # To run this application on your localhost you must first cd into the directory
 # containing MIT_Solve_dashboard.py and then in your terminal run 
 # $ streamlit run MIT_Solve_dashboard.py
@@ -12,21 +13,6 @@ import plotly.express as px
 # Title for page
 st.title('Matching Page for MIT Solve')
 
-# Below are place holders for the top 4 mentor matches
-# More thought must go into what information we want to represent about them besides output score
-# Need to figure out where to dynamically place output score to the left of mentor description and not within it
-
-st.write("### Here is where the best mentor match will go ---------- OUTPUT SCORE: 96\n"
-"the rest of the info we want to put will go here")
-
-st.write("### Here is where the second best mentor match will go ---- OUTPUT SCORE: 87\n"
-"the rest of the info we want to put will go here")
-
-st.write("### Here is where the third best mentor match will go ---- OUTPUT SCORE: 82\n"
-"the rest of the info we want to put will go here")
-
-st.write("### Here is where the fourth best mentor match will go ---- OUTPUT SCORE: 76\n"
-"the rest of the info we want to put will go here")
 
 # This method turns any csv file into a panda dataframe
 #@st.cache
@@ -51,6 +37,38 @@ selected_mentor = st.sidebar.selectbox(
     'Select a Mentor',
      list(total_score_df['Unnamed: 0']))
 
+
+# This gets all mentors top 4 including ties
+st.title("Top Four Matches Including Ties")
+top_four_with_ties = total_score_df.nlargest(4, selected_solver, "all")
+labeled_top_four_with_ties = top_four_with_ties[['Unnamed: 0', selected_solver]].set_index("Unnamed: 0")
+#st.write(labeled_top_four_with_ties)
+
+# Below are place holders for the top 4 mentor matches
+# More thought must go into what information we want to represent about them besides output score
+# Need to figure out where to dynamically place output score to the left of mentor description and not within it
+
+ranking = 0
+ties = 0
+previous_value = 0
+
+# This for loop ranks the top 4 mentors with numbers on the left hand side
+for mentor in labeled_top_four_with_ties.index:
+  total_output_value = labeled_top_four_with_ties[selected_solver][mentor]
+  if (previous_value == total_output_value):
+    ties += 1
+  else:
+    ranking += ties
+    ties = 0
+    ranking+=1
+    previous_value = total_output_value
+  st.write(str(ranking) + ".   Mentor: " + mentor + " OUTPUT SCORE: " + str(total_output_value))
+  
+  
+  #print(labeled_top_four_with_ties[selected_solver][mentor])
+
+
+
 # Creates the pylotly express bar chart that will be displayed
 # TODO Want to make this look prettier
 mentors = list(total_score_df['Unnamed: 0'])
@@ -65,7 +83,7 @@ bar_chart_to_display.update_layout(yaxis={'categoryorder':'total ascending'})
 st.write(bar_chart_to_display)
 
 # This gets all mentors with the highest value including ties
-st.write(total_score_df)
+#st.write(total_score_df.set_index("Unnamed: 0"))
 top_values = total_score_df[total_score_df[selected_solver] == total_score_df[selected_solver].max()]
 #st.write(top_values['Unnamed: 0'])
 
@@ -75,10 +93,7 @@ top_four = ordered_df[:4]['Unnamed: 0']
 #top_four = ordered_df[:4].index
 #st.write(top_four)
 
-# This gets all mentors top 4 including ties
-st.title("Top Four Matches Including Ties")
-top_four_with_ties = total_score_df.nlargest(4, selected_solver, "all")
-st.write(top_four_with_ties)
+
 
 
 # selected_row_for_chart = total_score_df[total_score_df['Org']==selected_solver]
@@ -88,9 +103,9 @@ st.write(top_four_with_ties)
 
 # Display more information about the selected solver
 solver_needs_df = csv_to_df("excel_to_csv/solver_team_data.csv")
-selected_solver_row_info = solver_needs_df[solver_needs_df['Org']==selected_solver]
+selected_solver_row_info = solver_needs_df[solver_needs_df['Org']==selected_solver].dropna(axis='columns')
 st.title("More information on " + selected_solver)
-st.write(selected_solver_row_info)
+st.table(selected_solver_row_info.set_index("Org"))
 
 
 # TODO click on bargraph/label and get display more info of mentor
@@ -98,4 +113,4 @@ st.write(selected_solver_row_info)
 mentor_data_df = csv_to_df("excel_to_csv/partner_data.csv")
 selected_mentor_row_info = mentor_data_df[mentor_data_df['Org']==selected_mentor]
 st.title("More information on " + selected_mentor)
-st.write(selected_mentor_row_info)
+st.table(selected_mentor_row_info.set_index("Org"))
